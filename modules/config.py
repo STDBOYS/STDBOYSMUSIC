e9f6ac7c87984bfb0ad780d0e1c91b97ef9e648e
STDBOYS <107495635+STDBOYS@users.noreply.github.com>
2022-06-16 20:37:59 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -4,13 +4,13 @@ import aiohttp
 from os import getenv
 from dotenv import load_dotenv
 
-# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫)
+# 𝐈𝐧𝐭𝐞𝐫𝐧𝐚𝐥 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 (@STD_DEEPANSHU)
 load_dotenv()
 que = {}
 admins = {}
 aiohttpsession = aiohttp.ClientSession()
 
-# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @𝐀𝐝𝐢𝐭𝐲𝐚𝐇𝐚𝐥𝐝𝐞𝐫
+# 𝐑𝐞𝐪𝐮𝐢𝐫𝐞𝐝 𝐕𝐚𝐫𝐢𝐚𝐛𝐥𝐞𝐬 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 @STD_DEEPANSHU
 API_HASH = getenv("API_HASH", "XXXXX")
 API_ID = int(getenv("API_ID", "XXXXX"))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "XXXXX")
@@ -20,7 +20,7 @@ BOT_NAME = getenv("BOT_NAME", "XXXXX")
 BOT_TOKEN = getenv("BOT_TOKEN", "12345:XXXXX")
 BOT_USERNAME = getenv("BOT_USERNAME", "XXXXX")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
-OWNER_NAME = getenv("OWNER_NAME", "𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫")
+OWNER_NAME = getenv("OWNER_NAME", "𝙎𝙏𝘿 𝘽𝙊𝙔𝙎")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "AdityaHalder")
 SOURCE_CODE = getenv("SOURCE_CODE", "https://github.com/adityabots/tgmusicbot")
 STRING_SESSION = getenv("STRING_SESSION", "session")
@@ -28,6 +28,6 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5356564375").split()))
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AdityaDiscus")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/AdityaServer")
 
-# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝐀𝐝𝐢𝐭𝐲𝐚 𝐇𝐚𝐥𝐝𝐞𝐫) 
+# 𝐃𝐨 𝐍𝐨𝐭 𝐂𝐡𝐚𝐧𝐠𝐞 𝐓𝐡𝐢𝐬 𝐋𝐢𝐧𝐞𝐬 // 𝐈𝐠𝐧𝐨𝐫𝐞 𝐓𝐡𝐢𝐬 (𝙎𝙏𝘿 𝘽𝙊𝙔𝙎 ) 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 PROFILE_CHANNEL = getenv("PROFILE_CHANNEL", "https://t.me/kaalxd")
